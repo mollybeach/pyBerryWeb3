97f28fd14973a7940d7d403fe56e0bc098092993
mollybeach <mollyjbeach@gmail.com>
2021-10-10 20:13:15 -0700

    get_etherscan_api_response save_etherscan_api_response to json file

--- api-call.py
+++ api-call.py
@@ -16,11 +16,19 @@ CHECKSUM_ADDRESS = Web3.toChecksumAddress(TREASURE_ADDRESS )
 url = f"https://api.etherscan.io/api?module=account&action=txlist&address=0x07edbd02923435fe2c141f390510178c79dbbc46&startblock=0&endblock=99999999&sort=asc&apikey=8DZ7FZZ3GEM1KN4SJAKDEF7KWQJHJKIA62"
 
 #make an api call to url and return the response and save to a json file ./etherscan_api_response.json
+
+def get_etherscan_api_response(url):
+    import requests
+    response = requests.get(url)
+    return response.json()
+
 def save_etherscan_api_response(url):
     response = get_etherscan_api_response(url)
     with open('./etherscan_api_response.json', 'w') as outfile:
         json.dump(response, outfile)
         
+
+
 
 save_etherscan_api_response(url)
 
